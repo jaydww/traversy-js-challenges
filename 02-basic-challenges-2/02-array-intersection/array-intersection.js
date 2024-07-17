6ba59fef2b05c78572d9bf46750e37fea3df26f3
jaydww <jamesdwalsh@hotmail.com>
2024-07-16 20:38:44 -0500

    update

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+	let intersectArr = []
 
-module.exports = arrayIntersection;
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr2.includes(arr1[i]) && !intersectArr.includes(arr1[i])) {
+			intersectArr.push(arr1[i])
+		}
+	}
+
+	return intersectArr
+}
+
+module.exports = arrayIntersection
