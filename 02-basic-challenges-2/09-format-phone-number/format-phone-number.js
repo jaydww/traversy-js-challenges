ea259e8231b06948ca312d412d73a19517b5e3fe
jaydww <jamesdwalsh@hotmail.com>
2024-08-04 23:53:16 -0500

    new exercises

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,13 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+	const areaCode = `(${arr[0]}${arr[1]}${arr[2]})`
 
-module.exports = formatPhoneNumber;
+	const start = `${arr[3]}${arr[4]}${arr[5]}`
+
+	const finish = `${arr[6]}${arr[7]}${arr[8]}${arr[9]}`
+
+	const fullNumber = `${areaCode} ${start}-${finish}`
+
+	return fullNumber
+}
+
+module.exports = formatPhoneNumber
--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,24 @@
-function validateEmail() {}
+function validateEmail(email) {
+	if (email.indexOf('@') === -1) {
+		return false
+	}
 
-module.exports = validateEmail;
+	const [localPart, domain] = email.split('@')
+
+	if (localPart.length === 0 || domain.length < 3) {
+		return false
+	}
+
+	const domainExtension = domain.split('.')
+
+	if (
+		domainExtension.length < 2 ||
+		domainExtension[domainExtension.length - 1].length < 2
+	) {
+		return false
+	}
+
+	return true
+}
+
+module.exports = validateEmail
